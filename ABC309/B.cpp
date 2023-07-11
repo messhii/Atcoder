de79e2e0eafe0779276c5d99c752110d54457bf3
messhii <messhiidada@gmail.com>
2023-07-12 08:03:57 +0900

    int型配列をstring型に変更

--- ABC309/B.cpp
+++ ABC309/B.cpp
@@ -4,26 +4,16 @@ using namespace std;
  
 int main(){
   int N;
-  char a[MAX][MAX];
+  string A[MAX], B[MAX];
   cin >> N;
-  for(int i=0; i<N; i++) for(int j=0; j<N; j++) cin >> a[i][j];
-  for(int i=0; i<N; i++){
-    for(int j=0; j<N; j++){
-      if(i == 0){
-        if(j == 0) cout << a[1][0];
-        else cout << a[i][j-1];
-      }else if(i == N-1){
-        if(j == N-1) cout << a[N-2][N-1];
-        else cout << a[i][j+1];
-      }else if(j == 0){
-        cout << a[i+1][j];
-      }else if(j == N-1){
-        cout << a[i-1][j];
-      }else{
-        cout << a[i][j];
-      }
-    }
-    cout << endl;
+  for(int i=0; i<N; i++) cin >> A[i];
+
+  B[0] = A[1].at(0) + A[0].substr(0, N-1);
+  for(int i=1; i<N-1; i++){
+    B[i] = A[i+1].at(0) + A[i].substr(1, N-2) + A[i-1].at(N-1);
   }
+  B[N-1] = A[N-1].substr(1, N-1) + A[N-2].at(N-1);
+
+  for(int i=0; i<N; i++) cout << B[i] << endl;
   return 0;
 }
