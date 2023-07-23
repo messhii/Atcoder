1d0e9cc649425035a3c0a8dbfeb9419bdbd23934
messhii <messhiidada@gmail.com>
2023-07-23 20:10:43 +0900

    for文の順番を変更、使用する変数を削除

--- ABC296/B.cpp
+++ ABC296/B.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <string>
-using namespace std;
 #define N 8
+using namespace std;
  
 int main(){
     string s;
-    int i_ast = 0, j_ast = 0;
-    for(int i=0; i<N; i++){
+    for(int i=N; i>0; i--){
         cin >> s;
         for(int j=0; j<N; j++){
-            if(s.at(j) == '*') i_ast = i, j_ast = j;
+            if(s.at(j) == '*'){
+                cout << (char)('a' + j) << i << endl;
+                return 0;
+            }
         }
     }
-    cout << (char)('a' + j_ast) << N - i_ast << endl;
-    return 0;
-}
+    return 0;}
