9d4bafa8d8e8f3b309d7d938d050c83c8a6bf8be
messhii <messhiidada@gmail.com>
2023-07-23 21:18:30 +0900

    変数名を変更、使用する変数を減らした

--- ABC303/B.cpp
+++ ABC303/B.cpp
@@ -3,28 +3,28 @@
 using namespace std;
  
 int main(){
-  int n, m, flag, res=0;
-  int a[50][50], pos[50][50];
+  int n, m, flag, ans=0;
+  int a, pos[50][50];
   cin >> n >> m;
   for(int i=0; i<m; i++){
     for(int j=0; j<n; j++){
-      cin >> a[i][j];
-      pos[i][a[i][j]] = j;
+      cin >> a;
+      pos[i][a] = j;
     }
   }
   
-  for(int a2=1; a2<=n; a2++){
-    for(int a3=a2+1; a3<=n; a3++){
+  for(int j=1; j<=n; j++){
+    for(int j2=j+1; j2<=n; j2++){
       flag = 0;
       for(int i=0; i<m; i++){
-        if(abs(pos[i][a2]-pos[i][a3]) == 1) {
+        if(abs(pos[i][j]-pos[i][j2]) == 1) {
           flag = 1;
           break;
         }
       }
-      if(!flag) res++;
+      if(!flag) ans++;
     }
   }
-  cout << res << endl;
+  cout << ans << endl;
   return 0;
 }
