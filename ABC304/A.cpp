c3efd809b2a84a301dacecc5847cb2d86662cf65
messhii <messhiidada@gmail.com>
2023-07-24 23:40:21 +0900

    剰余演算子(%)を用いる方法に変更

--- ABC304/A.cpp
+++ ABC304/A.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 #define MAX 100
  
 int main(){
-  int N, a[MAX], i=0;
-  string s[MAX];
-  int min_age=1e9, min_i=0;
-  bool check[MAX];
-    
+  int N, min_i=0;
   cin >> N;
-  for(i=0; i<N; i++){
+  vector<int> a(N);
+  vector<string> s(N);
+
+  for(int i=0; i<N; i++){
     cin >> s[i] >> a[i];
-	if(a[i] < min_age){
-      min_age = a[i];
-      min_i = i;
-    }
+	  if(a[i] < a[min_i]) min_i = i;
   }
-  i = min_i;
-  while(!check[i]){
-	cout << s[i] << endl;    
-	check[i] = true;    
-    if(i==N-1) i=0;
-    else i++;
+  for(int i=min_i; i<min_i+N; i++){
+    cout << s[i%N] << endl;
   }
   return 0;
 }
