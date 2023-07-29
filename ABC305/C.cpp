640b19683758d6639bad8a9bee6cb075250b1272
messhii <messhiidada@gmail.com>
2023-07-29 18:39:36 +0900

    Accepted

--- ABC305/C.cpp
+++ ABC305/C.cpp
@@ -5,27 +5,29 @@ using namespace std;
  
 int main(){    
     int h, w;
-    int flag = 0, cnt = 0, ans_i = 0, ans_j = 0;
+    int cnt = 0, cnt2 = 0, ans_i = 0, comp_i = 0;
     cin >> h >> w;
     vector<string> s(h);
 
     for(int i=0; i<h; i++) cin >> s[i];
 
-    for(int i=0; i<h-1; i++){
-        cnt = 0, flag = 0;
+    for(int i=0; i<h; i++){
+        cnt = cnt2;
+        cnt2 = 0;
         for(int j=0; j<w; j++){
-            if(s[i][j] != s[i+1][j]){
-                cnt++;
-                cout << "i " << i << " j " << j << endl;
-                if(cnt > 1) break;
-                if(!flag && s[i][j] == '.'){
-                    ans_i = i;
-                    ans_j = j;
-                    flag = 1;
-                }
-            }
+            if(s[i][j] == '#') cnt2++;
+        }
+        if(cnt > 0 && cnt2 > 0 && abs(cnt-cnt2) == 1){
+            ans_i = cnt < cnt2 ? i - 1 : i;
+            comp_i = cnt < cnt2 ? i : i - 1;
+        }
+        
+    }
+    for(int j=0; j<w; j++){
+        if(s[ans_i][j] != s[comp_i][j]){
+            cout << ans_i + 1 << " " << j + 1 << endl;
+            break;
         }
     }
-    cout << ans_i + 1 << " " << ans_j + 1 << endl;
     return 0;
 }
