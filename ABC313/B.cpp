3032734fe0275c90762af33d7df8cf3a2191ca5b
messhii <messhiidada@gmail.com>
2023-08-08 20:26:26 +0900

    Accepted

--- ABC313/B.cpp
+++ ABC313/B.cpp
@@ -1,34 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
  
 int main(){
-    int n, m, a, b, a1, next;
+    int n, m, w, l, ans, cnt = 0;
     cin >> n >> m;
-
-    vector<queue<int>> q(n+1);
-    vector<int> win(n+1, 0);
-    queue<int> q2;
+    vector<int> check(n, 0);
 
     for(int i=0; i<m; i++){
-        cin >> a >> b;
-        if(i == 0) a1 = a;
-        q[a].push(b);
+        cin >> w >> l;
+        check[l-1] = 1;
     }
-    q2 = q[a1];
-    while(!q2.empty()){
-        next = q2.front();
-        q2.pop();
-        win[next] = 1;
-        if(!q[next].empty()) q2 = q[next];
-    }
-    for(int i=1; i<=n; i++){
-        if(i != a1 && win[i] == 0){
+    for(int i=0; i<n; i++){
+        if(!check[i]) {
+            ans = i + 1;
+            cnt++;
+        }
+        if(cnt > 1){
             cout << -1 << endl;
             return 0;
         }
     }
-    cout << a1 << endl;
+    cout << ans << endl;
     return 0;
 }
