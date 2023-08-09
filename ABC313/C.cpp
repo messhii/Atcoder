38ab37c2e32db7349c5f861766bff9af294b001a
messhii <messhiidada@gmail.com>
2023-08-10 07:50:17 +0900

    Accepted

--- ABC313/C.cpp
+++ ABC313/C.cpp
@@ -4,24 +4,18 @@ using namespace std;
  
 int main(){
     int n;
-    long sum = 0, sum2, ave, ans;
+    long sum = 0, ans = 0, ans2 = 0, ave, ave2;
     cin >> n;
     vector<int> a(n);
     for(int i=0; i<n; i++) cin >> a[i], sum += a[i];
     ave = sum / n;
+    ave2 = sum % n > 0 ? ave + 1 : ave;    
 
-    if(sum % 2 == 1){
-        sum = 0, sum2 = 0;
-        for(int i=0; i<n; i++){
-            if(a[i] > ave + 1) sum += (a[i] - (ave + 1));
-            if(a[i] < ave) sum2 += (ave - a[i]);
-        }
-        ans = sum > sum2 ? sum : sum2;
-    }else{
-        for(int i=0; i<n; i++){
-            if(a[i] > ave) ans += (a[i] - ave);
-        }
+
+    for(int i=0; i<n; i++){
+        if(ave > a[i]) ans += ave - a[i];
+        else if(ave2 < a[i]) ans2 += a[i] - ave2;
     }
-    cout << ans << endl;
+    cout << (ans > ans2 ? ans : ans2)  << endl;
     return 0;
 }
