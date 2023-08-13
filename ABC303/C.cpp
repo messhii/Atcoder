26b6cd0648b21063efc4952485394a9c74416b0c
messhii <messhiidada@gmail.com>
2023-08-13 16:34:13 +0900

    アイテムの位置を１次元配列での管理に変更

--- ABC303/C.cpp
+++ ABC303/C.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
-#include <cmath>
+#include <string>
+#include <vector>
 #include <map>
 using namespace std;
- 
-#define MAX 200000
- 
+
 int main(){
-  int n, m, h, k;
-  int x[MAX], y[MAX]; // アイテムの場所
-  map<int, map<int, bool>> item;
-  int x2=0, y2=0;  // 現在地
-  int n_cnt = 0;
-  char s[MAX];
+  int n, m, h, k, x, y;
+  string s;
   cin >> n >> m >> h >> k >> s;
+  map<int, int> item;
+
   for(int i=0; i<m; i++){
-    cin >> x[i] >> y[i];
-	item[x[i]][y[i]] = true; 
+    cin >> x >> y;
+    item[x] = y;
   }
+  x = 0, y = 0;
   for(int i=0; i<n; i++){
-    if(s[i] == 'R') x2++;
-    else if(s[i] == 'L') x2--;
-    else if(s[i] == 'U') y2++;
-    else if(s[i] == 'D') y2--;
-    h--, n_cnt++;
+    h--;
     if(h<0){
       cout << "No" << endl;
       return 0;
     }
-    if(h<k && item[x2][y2]){
-      item[x2][y2] = false;
-      h=k;
+    if(s[i] == 'R') x++;
+    else if(s[i] == 'L') x--;
+    else if(s[i] == 'U') y++;
+    else if(s[i] == 'D') y--;
+    if(h < k && item[x] == y){
+      h = k;
+      item.erase(x);
     }
   }
   cout << "Yes" << endl;
