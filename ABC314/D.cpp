0720b1413b9953104db6b9807dfdae77bf631a04
messhii <messhiidada@gmail.com>
2023-08-13 14:44:23 +0900

    pairで最後の状態を記憶するように変更

--- ABC314/D.cpp
+++ ABC314/D.cpp
@@ -1,48 +1,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <utility>
 using namespace std;
  
 int main(){
-    int n, q, t, x, flag;
-    int i2 = 0, t2 = 1;
+    int n, q, t, x;
     char c;
     string s;
     cin >> n >> s >> q;
     vector<int> x2(q);
-
+    vector<char> c2(q);
+    pair<int, int> end_type(0, 0);
     for(int i=0; i<q; i++){
         cin >> t >> x >> c;
         x2[i] = x - 1;
-        if(t == 1) s[x-1] = c;
-        else i2 = i, t2 = t;
+        c2[i] = c;
+        if(t == 1) s[x2[i]] = c2[i];
+        else if(t == 2 || t == 3) end_type = make_pair(i, t);
     }
-    if(t2 == 2){
-        for(int i=0; i<n; i++) {
-            flag = 0;
-            for(int j=i2; j<q; j++){
-                if(i == x2[j]) {
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag) continue;
-            s[i] = tolower(s[i]);
-        }
-    }
-    else if(t2 == 3){
-        for(int i=0; i<n; i++) {
-            flag = 0;
-            for(int j=i2; j<q; j++){
-                if(i == x2[j]) {
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag) continue;
-            s[i] = toupper(s[i]);
-        }
-
+    if(end_type.first > 0){
+        if(end_type.second == 2) for(int i=0; i<n; i++) s[i] = tolower(s[i]);
+        else for(int i=0; i<n; i++) s[i] = toupper(s[i]);
+        for(int i=end_type.first + 1; i<q; i++) s[x2[i]] = c2[i];
     }
     cout << s << endl;
     return 0;
