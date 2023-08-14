bd000f8dbe9b8fbb845f6e038e6f1932af7789a4
messhii <messhiidada@gmail.com>
2023-08-15 07:37:06 +0900

    vector<string>に変更

--- ABC300/B.cpp
+++ ABC300/B.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
- 
  
 int main(){
   int h, w, flag=0;
-  char a[31][31], b[31][31], tmp;
   cin >> h >> w;
-  for(int i=0; i<h; i++) for(int j=0; j<w; j++) cin >> a[i][j];
-  for(int i=0; i<h; i++) for(int j=0; j<w; j++) cin >> b[i][j];  
+  vector<string> a(h), b(h);
+
+  for(auto& e: a) cin >> e;
+  for(auto& e: b) cin >> e;
   
   for(int i=0; i<h; i++){
     for(int j=0; j<w; j++){
       flag = 0;
       for(int i2=0; i2<h; i2++){
         for(int j2=0; j2<w; j2++){
-          if(a[(i+i2) % h][(j+j2) % w]!=b[i2][j2]){
+          if(a[(i+i2) % h][(j+j2) % w] != b[i2][j2]){
             flag = 1;
             break;
           }
