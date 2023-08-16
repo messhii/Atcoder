f6cb1ea0cfcfa16a7d39ae01756af361a63cd614
messhii <messhiidada@gmail.com>
2023-08-17 08:14:00 +0900

    Accepted

--- ABC299/B.cpp
+++ ABC299/B.cpp
@@ -1,42 +1,22 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define N 200000
+
 int main(){
-  long n, t, c, r;
-  int c1 = 0, max = 0, flag = 0, res;
-  long c_1[N], c_t[N];
-  cin >> n >> t;
-  for(int i=0; i<n; i++){
-    cin >> c;
-    if(i==0) c1 = c;
-    if(c==c1) c_1[i] = 1;
-    else if(c==t){
-      c_t[i] = 1;
-      flag = 1;
+    int n, t, t1, max = 0;
+    cin >> n>> t;
+    vector<int> c(n), r(n);
+    int flag = 0;
+    for(int i=0; i<n; i++){
+        cin >> c[i];
+        if(i == 0) t1 = c[i];
+        if(flag == 0 && c[i] == t) max = i, flag = 1;
     }
-  }
-  if(flag){
-    for(int i=0; i<n; i++){	
-      cin >> r;
-      if(c_t[i]){
-        if(r>max){
-          max = r;
-          res = i + 1;
-        }
-      }
-    }
-  }else{
-    for(int i=0; i<n; i++){	
-      cin >> r;
-      if(c_1[i]){
-        if(r>max){
-          max = r;
-          res = i + 1;
-        }
-      }
-    }
-  }
-  cout << res << endl;
-  return 0;
+    if(!flag) t = t1;
+    for(int i=0; i<n; i++){
+        cin >> r[i];
+        if(c[i] == t && r[max] < r[i]) max = i;
+    }    
+    cout << max + 1 << endl;
+    return 0;
 }
