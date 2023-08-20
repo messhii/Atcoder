498e8a277f5abbd419314b0664c7dcb88345f1fc
messhii <messhiidada@gmail.com>
2023-08-20 13:10:22 +0900

    大きい値を固定してfor文使用に変更

--- ABC315/C.cpp
+++ ABC315/C.cpp
@@ -5,34 +5,18 @@
 using namespace std;
  
 int main(){
-    int n, f, s, ans = 0, flag = 0;
+    int n, f, s, ans = 0, tmp;
     cin >> n;
     vector<pair<int, int>> ice(n);
-    for(int i=0; i<n; i++){
-        cin >> f >> s;
-        ice[i] = make_pair(s, f);
-    }
+    for(auto& [s, f]: ice) cin >> f >> s;
     sort(ice.rbegin(), ice.rend());
-
-    if(ice[0].second == ice[1].second) ans = ice[0].first + ice[1].first / 2;
-    ice.erase(unique(ice.begin(), ice.end()), ice.end());
-    if(ice.size() > 1){
-        for(int i=0; i<ice.size(); i++){
-            for(int j=i+1; j<ice.size(); j++){
-                if(ice[i].first + ice[j].first > ans){
-                    if(ice[i].second == ice[j].second) continue;
-                    else{
-                        ans = ice[i].first + ice[j].first;
-                        flag = 1;
-                        break;
-                    }
-                }else{
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag) break;
+    for(int i=1; i<n; i++){
+        if(ice[0].second == ice[i].second){
+            tmp = ice[0].first + ice[i].first / 2;
+        }else{
+            tmp = ice[0].first + ice[i].first;
         }
+        if(ans < tmp) ans = tmp;
     }
     cout << ans << endl;
     return 0;
