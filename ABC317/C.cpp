3c5f7f0c595d183bc284dc392b90f927fc8d790b
messhii <messhiidada@gmail.com>
2023-08-28 23:26:29 +0900

    DFS(再帰)を使用 → Accepted

--- ABC317/C.cpp
+++ ABC317/C.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <cmath>
 using namespace std;
- 
-int main(){
-    int n, m;
-    queue<int> q;
-    cin >> n >> m;
-    
-    vector<int> detected(n);
-    for(int i=0; i<n; i++){
-        detected[i] = true;
-        q.push(i);
-        while(!q.empty()){
-            int c = q.front();
-            q.pop();
-            for(int i=0; i<n; i++){
-                
-            }
-        }
+
+int n, m;
+int d[15][15], ans = 0;
+bool visited[15];
+
+void dfs(int x, int sum){
+    ans = max(ans, sum);
+    visited[x] = true;
+    for(int i=1; i<=n; i++){
+        if(!visited[i] && d[x][i]) dfs(i, sum + d[x][i]);
     }
+    visited[x] = false;
+}
+
+int main(){
+    int a, b, c;
+    cin >> n >> m;
+    for(int i=0; i<m; i++){
+        cin >> a >> b >> c;
+        d[a][b] = d[b][a] = c;
+    }
+    for(int i=1; i<=n; i++){
+        dfs(i, 0);
+    }
+    cout << ans << endl;
     return 0;
 }
