92233fb758fa9a7513492865d23ef480d155189c
messhii <messhiidada@gmail.com>
2023-08-29 21:20:49 +0900

    DPで解く → accepted

--- ABC317/D.cpp
+++ ABC317/D.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
-#include <climits>
 #include <vector>
-#include <algorithm>
+#include <cmath>
 using namespace std;
- 
+
+typedef long long int ll;
+#define INF 1e18
+
 int main(){
-    int n, x, y, z;
-    int z1 = 0, z2 = 0;
-    long long ans = 0, min = LLONG_MAX;
+    int n, z_sum = 0;
     cin >> n;
-    vector<pair<int, int>> p;
-    for(int i=0; i<n; i++){
-        cin >> x >> y >> z;
-        if(x > y) z1 += z;
-        else{
-            z2 += z;
-            p.push_back(make_pair((y - x)/2 + 1, z));
-        }
+    vector<int> x(n + 1), y(n + 1), z(n + 1);
+    for(int i=1; i<=n; ++i){
+        cin >> x[i] >> y[i] >> z[i];
+        z_sum += z[i];
     }
-    if(z1 < z2){
-        sort(p.begin(), p.end());
-        for(int i=0; i<p.size(); i++){
-            long long temp = 0;
-            z = 0;
-            for(int j=i; j<p.size(); j++){
-                temp += p[j].first;
-                z += p[j].second;
-                if(z1 + z > z2 - z){
-                    if(min > temp) min = temp;
-                    break;
-                }
+    vector<vector<ll>> dp(n+1, vector<ll>(z_sum+1, INF));
+
+    dp[0][0] = 0;
+    for(int i=0; i<=n; i++) dp[i][0] = 0;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=z_sum; j++){
+            int w;
+            if(x[i] > y[i]) w = 0;
+            else w = (y[i] - x[i]) / 2 + 1; // w, z[i]を使う
+            if(j >= z[i]){
+                dp[i][j] = min(dp[i-1][j-z[i]]+w, dp[i-1][j]);
+            }else{
+                dp[i][j] = dp[i-1][j];
             }
         }
-        ans = min;
+    }
+    ll ans = INF;
+    for(int j= z_sum / 2 + 1; j <= z_sum; ++j){
+        ans = min(ans, dp[n][j]);
     }
     cout << ans << endl;
     return 0;
