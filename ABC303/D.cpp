85f1c1d2593246e3f5e1a298c9708a5c82bd7c78
messhii <messhiidada@gmail.com>
2023-08-30 23:31:21 +0900

    DPで解く → Accepted

--- ABC303/D.cpp
+++ ABC303/D.cpp
@@ -1,39 +1,30 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
-
-long long addTime(long long a, long long b, long long c, int cnt, bool* capsLock){
-    long long res = 0;
-    if(a * cnt < c + (b * cnt)) res += a * cnt;
-    else{
-        res += c + (b * cnt);
-        *capsLock = !(*capsLock);
-    }
-    return res;
-}
+typedef long long ll;
 
 int main(){
-    long long x, y, z, ans = 0;
-    bool capsLook = false;
+    int x, y, z;
     string s;
     cin >> x >> y >> z >> s;
-    s.push_back('x');
-    char state = s[0];
-    int cnt = 1;
-    for(int i=1; i<s.length(); i++){
-        if(state == s[i]) cnt++;
-        else{
-            if(capsLook == false){
-                if(s[i-1] == 'a') ans += addTime(x, y, z, cnt, &capsLook);
-                else ans += addTime(y, x, z, cnt, &capsLook);
-            }else{
-                if(s[i-1] == 'a') ans += addTime(y, x, z, cnt, &capsLook);
-                else ans += addTime(x, y, z, cnt, &capsLook);
-            }
-            state = s[i];
-            cnt = 1;
+    vector<vector<ll>> dp(s.size(), vector<ll>(2));
+    if(s[0] == 'a'){
+        dp[0][0] = x;
+        dp[0][1] = min(x + z, y + z);
+    }else{
+        dp[0][0] = y;
+        dp[0][1] = min(x + z, y + z);
+    }
+    for(int i=1; i<s.size(); i++){
+        if(s[i] == 'a'){
+            dp[i][0] = min(dp[i-1][0] + x, dp[i-1][1] + min(x + z, y + z));
+            dp[i][1] = min(dp[i-1][0] + min(x + z, y + z), dp[i-1][1] + y);
+        }else{
+            dp[i][0] = min(dp[i-1][0] + y, dp[i-1][1] + min(x + z, y + z));
+            dp[i][1] = min(dp[i-1][0] + min(x + z, y + z), dp[i-1][1] + x);
         }
     }
-    cout << ans << endl;
+    cout << min(dp[s.size()-1][0], dp[s.size()-1][1]) << endl;
     return 0;
 }
