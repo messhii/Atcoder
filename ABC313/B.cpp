d8d79735585937b1e5db8e189725f0f69e6465c5
messhii <messhiidada@gmail.com>
2023-09-01 06:21:41 +0900

    コード整理

--- ABC313/B.cpp
+++ ABC313/B.cpp
@@ -5,22 +5,17 @@ using namespace std;
 int main(){
     int n, m, w, l, ans, cnt = 0;
     cin >> n >> m;
-    vector<int> check(n, 0);
+    vector<int> check(n + 1, 0);
 
     for(int i=0; i<m; i++){
         cin >> w >> l;
-        check[l-1] = 1;
+        check[l] = 1;
     }
-    for(int i=0; i<n; i++){
-        if(!check[i]) {
-            ans = i + 1;
-            cnt++;
-        }
-        if(cnt > 1){
-            cout << -1 << endl;
-            return 0;
-        }
+    for(int i=1; i<=n; i++){
+        if(!check[i]) ans = i, cnt++;
+        if(cnt > 1) break;
     }
-    cout << ans << endl;
+    if(cnt == 1) cout << ans << endl;
+    else cout << -1 << endl;
     return 0;
 }
