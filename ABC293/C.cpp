347fda77ad5091371c184607a651b7ff96f3efc0
messhii <messhiidada@gmail.com>
2023-10-15 16:29:08 +0900

    変数名を修正

--- ABC293/C.cpp
+++ ABC293/C.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 #include <map>
 using namespace std;
-#define N 12
-int a[N][N];
+
 int h, w;
+int a[11][11];
+map<int, int> visited;
 int ans = 0;
-map<int, int> mp;
 
 void dfs(int x, int y){
-    if(x == w && y == h){
-        ans++;
-        return;
-    }
-    if(x+1 <= w && !mp[a[x+1][y]]){
-        mp[a[x+1][y]] = 1;
+    if(x == w && y == h) ans++;
+    if(x+1 <= w && !visited[a[y][x+1]]){
+        visited[a[y][x+1]] = 1;
         dfs(x+1, y);
-        mp[a[x+1][y]] = 0;
+        visited[a[y][x+1]] = 0;
     }
-    if(y+1 <= h && !mp[a[x][y+1]]){
-        mp[a[x][y+1]] = 1;
+    if(y+1 <= h && !visited[a[y+1][x]]){
+        visited[a[y+1][x]] = 1;
         dfs(x, y+1);
-        mp[a[x][y+1]] = 0;
+        visited[a[y+1][x]] = 0;
     }
+    return;
 }
 
 int main(){
     cin >> h >> w;
-    for(int i=1; i<=w; i++){
-        for(int j=1; j<=h; j++){
+    for(int i=1; i<=h; i++){
+        for(int j=1; j<=w; j++){
             cin >> a[i][j];
         }
     }
-    mp[a[1][1]] = 1;
+    visited[a[1][1]] = 1;
     dfs(1, 1);
     cout << ans << endl;
     return 0;
