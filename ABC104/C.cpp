3e200ce11f12207c83f451ac8edf91751cf154ca
messhii <messhiidada@gmail.com>
2023-10-18 22:08:58 +0900

    DPで解く

--- ABC104/C.cpp
+++ ABC104/C.cpp
@@ -5,13 +5,12 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    vector<int> h(n);
+    vector<int> h(n), dp(n);
     for(int&e: h) cin >> e;
     int cnt = 0, ans = 0;
-    for(int i=0; i<n-1; i++){
-        if(h[i] >= h[i+1]) cnt++;
-        else cnt = 0;
-        ans = ans < cnt ? cnt : ans;
+    for(int i=1; i<n; i++){
+        if(h[i] <= h[i-1]) dp[i] = dp[i-1] + 1;
+        ans = max(ans, dp[i]);
     }
     cout << ans << endl;
     return 0;
