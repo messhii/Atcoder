db51107ff8e1cd5ad30fc5be02046e63bb95e187
messhii <messhiidada@gmail.com>
2023-12-09 19:27:53 +0900

    Update

--- ABC331/A.cpp
+++ ABC331/A.cpp
@@ -4,16 +4,12 @@ using namespace std;
 int main(){
     int M, D, y, m, d;
     cin >> M >> D >> y >> m >> d;
-    if(d + 1 > D){
-        d = 1;
-        if(m + 1 > M){
-            m = 1;
-            y += 1;
-        }else{
-            m += 1;
-        }
+    if(m == M && d == D){
+        y++, m =1, d = 1;
+    }else if(d == D){
+        m++, d = 1;
     }else{
-        d += 1;
+        d++;
     }
     cout << y << " " << m << " " << d << endl;
     return 0;
